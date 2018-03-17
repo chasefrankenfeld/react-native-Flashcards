f4c19eba4f9a87713ca816ddc1576fcee4b0af86
Chase Frankenfeld <chasefrankenfeld@gmail.com>
2018-03-17 15:53:46 +1100

    Add TabNavigator to the App

--- App.js
+++ App.js
@@ -1,24 +1,75 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+  StatusBar,
+  Platform
+} from 'react-native';
+// import { createStore, applyMiddleware } from 'redux';
+// import { Provider } from 'react-redux';
+// import reducer from "./reducers";
+import { Constants } from "expo";
+import { TabNavigator } from "react-navigation";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
+import Decks from "./components/Decks";
+import NewDeck from "./components/NewDeck";
 
-export default class App extends React.Component {
+function CustomStatusBar({ backgroundColor, ...props }) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }} >
+      <StatusBar
+        translucent
+        barStyle="light-content"
+        backgroundColor={backgroundColor}
+        {...props}
+      />
+    </View>
+  )
+}
+
+const Tabs = TabNavigator({
+  Decks: {
+    screen: Decks,
+    navigationOptions: {
+      tabBarLabel: "Decks",
+      tabBarIcon: ({ tintColor }) => <Ionicons name="ios-home" size={30} color={tintColor} />
+    }
+  },
+  NewDecks: {
+    screen: NewDeck,
+    navigationOptions: {
+      tabBarLabel: "New Deck",
+      tabBarIcon: ({ tintColor }) => <FontAwesome name="plus-square" size={30} color={tintColor} />
+    }
+  }
+},{
+  navigationOptions: {
+    header: null,
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? "#000" : "#fff",
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? "#fff" : "#000",
+      shadowOffset: {
+        height: 0
+      },
+      shadowRadius: 0,
+      shadowOpacity: 0,
+      elevation: 0,
+    }
+  }
+});
+
+export default class App extends Component {
+
   render() {
+
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <View style={{flex: 1}} >
+        <CustomStatusBar backgroundColor={"#000"}/>
+        <Tabs />
       </View>
     );
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
+};
